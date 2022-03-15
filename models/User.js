4b37c59cd8c90d8f2df892afa69d8b764ef43221
Rick Ansay <rick.ansay@gmail.com>
2022-03-14 22:09:57 -0600

    Correct file naming, debugging routing

--- models/User.js
+++ models/User.js
@@ -4,31 +4,32 @@ const sequelize = require('../config');
 class User extends Model {}
 
 User.init(
+    
     {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: UUIDV4,
-            primaryKey: true,
-        },
-        username: {
-            type: DataTypes.String,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.String,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            }
-        },
-        password: {
-            type:DataTypes.String,
-            allowNull: false,
-            validate: {
-                len: [6]
-            }
-        }
+        // id: {
+        //     type: DataTypes.UUID,
+        //     defaultValue: UUIDV4,
+        //     primaryKey: true,
+        // },
+        // username: {
+        //     type: DataTypes.String,
+        //     allowNull: false,
+        // },
+        // email: {
+        //     type: DataTypes.String,
+        //     allowNull: false,
+        //     unique: true,
+        //     validate: {
+        //         isEmail: true,
+        //     }
+        // },
+        // password: {
+        //     type:DataTypes.String,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [6]
+        //     }
+        // }
         }, {
             sequelize,
             timestamps: false,
