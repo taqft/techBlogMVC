4c4f146b5f1b08493920eb5296bd98111859ec7a
Rick Ansay <rick.ansay@gmail.com>
2022-03-16 21:29:18 -0600

    Fixed routing error causing server to fail

--- models/User.js
+++ models/User.js
@@ -6,30 +6,30 @@ class User extends Model {}
 User.init(
     
     {
-        // id: {
-        //     type: DataTypes.UUID,
-        //     defaultValue: UUIDV4,
-        //     primaryKey: true,
-        // },
-        // username: {
-        //     type: DataTypes.String,
-        //     allowNull: false,
-        // },
-        // email: {
-        //     type: DataTypes.String,
-        //     allowNull: false,
-        //     unique: true,
-        //     validate: {
-        //         isEmail: true,
-        //     }
-        // },
-        // password: {
-        //     type:DataTypes.String,
-        //     allowNull: false,
-        //     validate: {
-        //         len: [6]
-        //     }
-        // }
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: UUIDV4,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [6]
+            }
+        }
         }, {
             sequelize,
             timestamps: false,
