65ec7f865eb7fbfade7256bc81ba4d1f914a8ccf
Rick Ansay <rick.ansay@gmail.com>
2022-04-04 23:16:36 -0600

    Delete blog button not working, diag...

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,10 +2,20 @@ $(document).ready(function () {
   const titleField = $('#blog-title');
   const contentField = $('#blog-content');
   const addBlogBtn = $('#addBlogBtn');
+  const deleteBlogBtn = $('#deleteBlogBtn');
 
   addBlogBtn.on('click', async function (event) {
     event.preventDefault();
     await $.post('/api/blog', {
+      title: titleField.val(),
+      content: contentField.val(),
+    });
+    window.location.reload();
+  });
+
+  deleteBlogBtn.on('click', async function (event) {
+    console.log('Delete');
+    await $.delete('/api/blog', {
       title: titleField.val(),
       content: contentField.val(),
     });
